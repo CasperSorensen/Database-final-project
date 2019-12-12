d089975fea704346b658e20b8e417f887e492d1b
CasperSorensen <caspers@live.dk>
2019-12-12 16:13:20 +0100

    quick fix to trioninsertusers

--- DbScripts/Triggers/tri_OnUserInsert.sql
+++ DbScripts/Triggers/tri_OnUserInsert.sql
@@ -1,6 +1,6 @@
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
--- Name:         tri_OnCreditCardInsert.sql
+-- Name:         tri_OnUserInsert.sql
 -- 
 -- Purpose:      Creates a ON DELETE trigger on the TCreditCard table
 --               It adds the Tsuers old and the new values FROM the row
@@ -21,109 +21,88 @@
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
 CREATE
-OR ALTER TRIGGER trg_OnCreditCardInsert ON TCreditCard
+OR ALTER TRIGGER trg_OnUserInsert
+ON TUser
 AFTER
 INSERT
     AS BEGIN
     -- DECLARE ALL VARIABLES
-    DECLARE @before_nCreditCardId INT
     DECLARE @before_nUserId INT
-    DECLARE @before_cIBANCode VARCHAR(34)
-    DECLARE @before_dExpDate VARCHAR(4)
-    DECLARE @before_nCcv INT
-    DECLARE @before_cCardholderName VARCHAR(40)
-    DECLARE @before_nAmountSpent DECIMAL(2)
-    -- SET NEW VALUES
-    DECLARE @after_nCreditCardId INT
-    DECLARE @after_nUserId INT
-    DECLARE @after_cIBANCode VARCHAR(34)
-    DECLARE @after_dExpDate VARCHAR(4)
-    DECLARE @after_nCcv INT
-    DECLARE @after_cCardholderName VARCHAR(40)
-    DECLARE @after_nAmountSpent DECIMAL(2)
-    -- SET SYSTEM DATE
-    DECLARE @cStatementType VARCHAR(10)
-    DECLARE @dtExecutedAt DATETIME
+    DECLARE @before_cFirstName VARCHAR(20)
+    DECLARE @before_cSurname VARCHAR(20)
+    DECLARE @before_cAddress VARCHAR(60) 
+    DECLARE @before_cPhoneNo VARCHAR(8) 
+    DECLARE @before_cZipcode VARCHAR(4) 
+    DECLARE @before_nCity INT 
+    DECLARE @before_cEmail VARCHAR(60)    
+    DECLARE @before_nTotalAmount DECIMAL(2) 
+    DECLARE @after_nUserId INT      
+    DECLARE @after_cFirstName VARCHAR(20) 
+    DECLARE @after_cSurname VARCHAR(20) 
+    DECLARE @after_cAddress VARCHAR(60) 
+    DECLARE @after_cPhoneNo VARCHAR(8) 
+    DECLARE @after_cZipcode VARCHAR(4) 
+    DECLARE @after_nCity INT 
+    DECLARE @after_cEmail VARCHAR (60)
+    DECLARE @after_nTotalAmount DECIMAL(2)      
+    DECLARE @cStatementType VARCHAR(10) 
+    DECLARE @dtExecutedAt DATETIME 
     DECLARE @nDBMSId INT
-    DECLARE @cDBMSName NVARCHAR(128)
-    DECLARE @cHostId CHAR(8)
+    DECLARE @cDBMSName NVARCHAR(128) 
+    DECLARE @cHostId CHAR(8) 
     DECLARE @cHostName NVARCHAR(128)
-    -- SET BEFORE VARIABLES
-    SELECT
-        @before_nCreditCardId = NULL
-    SELECT
-        @before_nUserId = NULL
-    SELECT
-        @before_cIBANCode = NULL
-    SELECT
-        @before_dExpDate = NULL
-    SELECT
-        @before_nCcv = NULL
-    SELECT
-        @before_cCardholderName = NULL
-    SELECT
-        @before_nAmountSpent = NULL
-    -- SET AFTER VARIABLES
-    SELECT
-        @after_nCreditCardId = nCreditCardId
-    FROM
-        inserted
-    SELECT
-        @after_nUserId = nUserId
-    FROM
-        inserted
-    SELECT
-        @after_cIBANCode = cIBANCode
-    FROM
-        inserted
-    SELECT
-        @after_dExpDate = dExpDate
-    FROM
-        inserted
-    SELECT
-        @after_nCcv = nCcv
-    FROM
-        inserted
-    SELECT
-        @after_cCardholderName = cCardholderName
-    FROM
-        inserted
-    SELECT
-        @after_nAmountSpent = nAmountSpent
-    FROM
-        inserted
-    SELECT
-        @cStatementType = 'INSERT'
-    -- SET THE SYSTEM VARIABLES
-    SET
-    @dtExecutedAt = GETDATE()
-    SET
-    @nDBMSId = USER_ID()
-    SET
-    @cDBMSName = USER_NAME()
-    SET
-    @cHostId = HOST_ID()
-    SET
-    @cHostName = HOST_NAME()
-    -- CALL THE INSERT INTO TAUDITUSERS STORED PROCEDURE
-    EXEC pro_InsertIntoAuditCreditCard @before_nCreditCardId,
+    
+    SELECT @before_nUserId = NULL 
+    SELECT @before_cFirstName = NULL 
+    SELECT @before_cSurname = NULL 
+    SELECT @before_cAddress = NULL 
+    SELECT @before_cPhoneNo = NULL 
+    SELECT @before_cZipcode = NULL 
+    SELECT @before_nCity = NULL 
+    SELECT @before_cEmail = NULL 
+    SELECT @before_nTotalAmount = NULL 
+    
+    SELECT @after_nUserId = nUserId from inserted
+    SELECT @after_cFirstName = cFirstName from inserted 
+    SELECT @after_cSurname = cSurname from inserted 
+    SELECT @after_cAddress = cAddress from inserted 
+    SELECT @after_cPhoneNo = cPhoneNo from inserted 
+    SELECT @after_cZipcode = cZipCode from inserted 
+    SELECT @after_nCity = cCity from inserted 
+    SELECT @after_cEmail = cEmail from inserted 
+    SELECT @after_nTotalAmount = nTotalAmount from inserted 
+    SELECT @cStatementType = 'INSERT' 
+    
+    SET @dtExecutedAt = GETDATE() 
+    SET @nDBMSId = USER_ID() 
+    SET @cDBMSName = USER_NAME() 
+    SET @cHostId = HOST_ID() 
+    SET @cHostName = HOST_NAME() 
+
+EXEC pro_InsertIntoAuditUsers 
     @before_nUserId,
-    @before_cIBANCode,
-    @before_dExpDate,
-    @before_nCcv,
-    @before_cCardholderName,
-    @before_nAmountSpent,
-    @after_nCreditCardId,
-    @after_nUserId,
-    @after_cIBANCode,
-    @after_dExpDate,
-    @after_nCcv,
-    @after_cCardholderName,
-    @after_nAmountSpent,
-    @cStatementType,
-    @dtExecutedAt,
+    @before_cFirstName,
+    @before_cSurname,
+    @before_cAddress, 
+    @before_cPhoneNo, 
+    @before_cZipcode, 
+    @before_nCity, 
+    @before_cEmail,     
+    @before_nTotalAmount, 
+    @after_nUserId,     
+    @after_cFirstName, 
+    @after_cSurname, 
+    @after_cAddress, 
+    @after_cPhoneNo, 
+    @after_cZipcode, 
+    @after_nCity, 
+    @after_cEmail,
+    @after_nTotalAmount,      
+    @cStatementType, 
+    @dtExecutedAt, 
     @nDBMSId,
-    @cDBMSName,
-    @cHostId,
+    @cDBMSName, 
+    @cHostId, 
     @cHostName
+
 END;
